10fc3698f58affc9d0851a842315ee603f463573
eseedo <eseedo@gmail.com>
2017-10-16 09:21:57 +0800

    add new iOS 11 tutorials- currently updated to cha4

---
+++ iOS11/Projects/cha4/BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by eseedo on 10/11/17.
+//  Copyright © 2017 eseedo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert(){
+        
+        let alert = UIAlertController(title:"Hello Russia",
+                                      message:"梅西上演帽子戏法拯救阿根廷",
+                                      preferredStyle: .alert)
+        let action = UIAlertAction(title:"2018年不再无聊",style: .default,handler: nil)
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+
+    @IBAction func sliderMoved(_ slider: UISlider){
+        
+        print("滑动条的当前数值是： \(slider.value)")
+    }
+}
+
